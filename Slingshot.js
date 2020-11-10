324ea0ec001ff794f9b0fe9685f44fd3dee5ae56
Your Name <you@example.com>
2020-11-10 13:35:19 -0500

    sling

--- Slingshot.js
+++ Slingshot.js
@@ -8,6 +8,9 @@ class SlingShot{
         }
         this.pointB = pointB
         this.sling = Constraint.create(options);
+        this.Sling1 = loadImage("sprites/sling1.png");
+        this.Sling2 = loadImage("sprites/sling2.png");
+        this.Sling3 = loadImage("sprites/sling3.png");
         World.add(world, this.sling);
     }
 
@@ -16,11 +19,30 @@ class SlingShot{
     }
 
     display(){
+        image(this.Sling1,200, 20);
+        image(this.Sling2,170, 20);
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
+            push();
             strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            stroke(48, 22, 8);
+
+if(pointA.x >= 230 ){
+
+            line(pointA.x + 20, pointA.y, pointB.x + 10, pointB.y);
+            line(pointA.x + 20, pointA.y, pointB.x - 30, pointB.y + 3);
+            image(this.Sling3, pointA.x + 20, pointA.y - 10, 15, 30);
+
+}
+
+else{
+
+    line(pointA.x - 20, pointA.y, pointB.x - 10, pointB.y);
+    line(pointA.x - 20, pointA.y, pointB.x + 30, pointB.y - 3);
+    image(this.Sling3, pointA.x - 30, pointA.y - 10, 15, 30);
+}
+            pop();
         }
     }
     
